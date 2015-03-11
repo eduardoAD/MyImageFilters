eebb01488c49bdf3eb15e548b65b4b3ca9f5c7e7
Eduardo Alvarado Diaz <GLB-MXM0001@GLB-MXM0001.local>
2015-03-11 16:26:55 -0600

    Initial Commit

---
+++ MyImageFilters/MyImageFilters.h
@@ -0,0 +1,13 @@
+//
+//  MyImageFilters.h
+//  MyImageFilters
+//
+//  Created by Eduardo Alvarado Díaz on 3/11/15.
+//  Copyright (c) 2015 Organization. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyImageFilters : NSObject
+
+@end
---
+++ MyImageFilters/MyImageFilters.m
@@ -0,0 +1,13 @@
+//
+//  MyImageFilters.m
+//  MyImageFilters
+//
+//  Created by Eduardo Alvarado Díaz on 3/11/15.
+//  Copyright (c) 2015 Organization. All rights reserved.
+//
+
+#import "MyImageFilters.h"
+
+@implementation MyImageFilters
+
+@end
